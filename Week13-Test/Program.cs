605565df39011a5c269578bdc3167e26b3ab774d
Opilane TTHK <opilane@tthk.ee>
2021-11-23 16:30:15 +0200

    test

---
+++ Week13-Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Week13_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Radnom facts so interesting you'll say omg
+            Console.WriteLine(GetRandomFact());
+        }
+
+        public static string GetRandomFact()
+        {
+            string filePath = @"C:\Users\opilane\Samples\RandomFactsSoInteresting.txt";
+            string[] RandomFact = File.ReadAllLines(filePath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, RandomFact.Length);
+
+            return RandomFact[randomIndex];
+
+
+            
+
+
+
+        }
+    }
+}
